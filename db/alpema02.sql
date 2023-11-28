09b370ed202f664d6c74f70bd43f9a25d36d8265
alexator20 <90261293+alexator20@users.noreply.github.com>
2023-11-28 13:47:42 +0100

    actuañlizacion

--- db/alpema02.sql
+++ db/alpema02.sql
@@ -1,7 +1,12 @@
-CREATE TABLE `persona` (
-  `edad` int(11) NOT NULL,
-  `nombre` varchar(20) NOT NULL,
-  'estilo' varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `persona` (`edad`, `nombre`, 'estilo') VALUES (20, 'Alejandro Pérez', 'pop'), (18, 'Lucía Albir', 'regueton');
+USE alpema02;
+
+CREATE TABLE persona (
+  edad int(11) NOT NULL,
+  nombre varchar(20) NOT NULL,
+  estilo varchar(40) NOT NULL
+);
+
+INSERT INTO persona VALUES (20, 'Alejandro Pérez', 'pop');
+INSERT INTO persona VALUES (18, 'Lucía Albir', 'regueton');
+
--- public/index.php
+++ public/index.php
@@ -1,31 +1,59 @@
-<html>
-    <head>
-        <title>Evaluable 1 Docker</title>
-        
-    </head>
-    <body>
-        
-            <?php
-                echo "<h1>Examen Docker</h1>";
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+//require_once DIR."/config/config.json";
+try {
+    // Conexión a la base de datos utilizando PDO
+    //$pdo = new PDO("mysql:host=localhost;port=3306;dbname=todolist2;", "root", "root");
 
-                $conn = mysqli_connect('db', 'root', 'root', "midb");
-                $query = 'SELECT * FROM persona';
-                $result = mysqli_query($conn, $query);
+    //$pdo = new PDO("mysql:127.0.0.1=localhost;dbname=todolist2", "root", "root");
 
-                echo '<table class="table table-striped">';
-                echo 'ID NOMBRE';
-                while($value = $result->fetch_array(MYSQLI_ASSOC)){
-                    echo '<tr>';
-                    foreach($value as $element){
-                        echo '<td>' . $element . '</td>';
-                    }
-                    echo '</tr>';
-                }
-                echo '</table>';
+    // Configurar el modo de error de PDO para lanzar excepciones
 
-                $result->close();
-                mysqli_close($conn);
-            ?>
-        
-    </body>
-</html>
+    //$jsonString = file_get_contents('../config/config.json'); <- NO FUNCIONA YA QUE LOCALHOST SE ENCUENTRA EN SRC Y NO SE PUEDE SALIR
+
+  //  (/var/www/html/  esto es src     config/config.json)
+    $jsonString = file_get_contents('config.json');
+    // decodificar el json a un array
+    $config = json_decode($jsonString, true);
+
+     // Conexión a la base de datos utilizando PDO
+     $serverName = '';
+     $userName = '';
+     $password = '';
+     $dbName = '';
+
+
+
+    if ($config === null) {
+        die("Error al decodificar el JSON.");
+    }else{
+    // sacamos del json los valores
+    $serverName = $config["host"];
+    $userName = $config["user"];
+    $password = $config["pass"];
+    $dbName = $config["nombre_base_datos"];
+    }
+
+
+
+    $pdo = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Consulta SQL
+    $query = "SELECT * FROM persona";
+
+    // Preparar la consulta
+    $statement = $pdo->prepare($query);
+
+    // Ejecutar la consulta
+    $statement->execute();
+
+    // Obtener y mostrar los resultados
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        echo "NOMBRE: " . $row['nombre'] . " EDAD: " . $row['edad']. " ESTILO: " . $row['estilo'] . "<br>";
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
